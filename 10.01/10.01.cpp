cf12b43d5327a648e925ebf20ab971a8c91f7561
molodoi1337 <nniki@DESKTOP-8AHT603>
2023-03-11 15:59:47 +0300

    Добавьте файлы проекта.

---
+++ 10.01/10.01.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include "Stekl.h"
+
+using namespace std;
+
+int main() {
+	srand(time(NULL));
+
+	Stack a;
+	Stack b;
+	Stack c;
+
+	for (int i = 0; i < 10; i++) {
+		a.push(rand() % 40 + 85);
+	}
+
+	a.show();
+	b.clon(a);
+	b.show();
+
+	cout << endl << endl;
+
+	for (int i = 0; i < 10; i++) {
+		c.push((char)rand() % 40 + 85);
+	}
+
+	
+}
+
